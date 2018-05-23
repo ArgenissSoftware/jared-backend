b9da845b5527981c94e07eec0c85bcf65985a10e
Samuel Garcia <fnc.samuel.garcia@gmail.com>
2018-05-23 11:38:51 -0300

    fix constant

--- server/helper/validationArgenissEmail.js
+++ server/helper/validationArgenissEmail.js
@@ -1,6 +1,6 @@
 function checkArgenissFormatEmail(email){
     var errorMessage = "";
-    var domain = "@argeniss.com";
+    const domain = "@argeniss.com";
 
     if(!email || email == "" || email.indexOf(domain) < 0){
         errorMessage += `${email}: Email have a format incorrect.`;
