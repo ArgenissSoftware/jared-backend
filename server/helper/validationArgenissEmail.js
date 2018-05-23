f4b1add78fe9b31c7efd3321ed0a28b830bcb608
Samuel Garcia <fnc.samuel.garcia@gmail.com>
2018-05-23 14:44:31 -0300

    remove unused var

--- server/helper/validationArgenissEmail.js
+++ server/helper/validationArgenissEmail.js
@@ -1,5 +1,4 @@
 function checkArgenissFormatEmail(email){
-    var errorMessage = "";
     const re =  /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(argeniss)\.com$/;
 
     return re.test(email);
