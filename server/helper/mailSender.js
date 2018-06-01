8bd3d4d4310b4256cb12dbac4f8b6dffd9496539
matiassequeira <matiassequeirq@gmail.com>
2018-05-31 22:27:37 -0300

    Modified configuration and dependencies archives to automate nodemailer instalation. Created a generic mail sender function. Authentication with mail server is in a separated archive.

---
+++ server/helper/mailSender.js
@@ -0,0 +1,60 @@
+var MailConnection = require('../mailConnection');
+
+//TIP: Nodemailer is unicode friendly ✔
+//receiverList is an array of strings, which must as follows:
+//  "[NAME SURNAME] email"
+function mailSender (receiversList, subject, text, html){
+
+    var errorMessage = '';
+    var mailOptions={};
+    var transporter = MailConnection.transporter;
+    var user= MailConnection.user;
+
+
+    //check we have at least one destination mail, and put the first into mailOptions
+    if(receiversList.length > 0){
+        mailOptions.to= receiversList[0];
+    }
+    else {
+        return errorMessage = 'Destination mail not provided';
+
+    }
+
+    //get the rest of destinations if any
+    for (var i=1; i<receiversList.length; i++){
+        mailOptions.to= mailOptions.to + ", " + receiversList[i];
+    }
+
+    console.log('Enviando a: %s', mailOptions.to);
+
+    //set the rest of the mail options
+    mailOptions.subject=subject;
+    mailOptions.text=text;
+    mailOptions.html=html;
+
+    //this field doesn't need to be the user used for authentication with the service
+    mailOptions.from= user;
+
+    console.log('Sending email from %s to %s', mailOptions.from, mailOptions.to);
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            return errorMessage= "There was an error while sending the email. Check console log";
+        }
+
+        console.log('Message sent: %s', info.messageId);
+
+
+
+    });
+
+    console.log(errorMessage);
+    return errorMessage;
+}
+
+
+module.exports = {
+    mailSender: mailSender
+}
