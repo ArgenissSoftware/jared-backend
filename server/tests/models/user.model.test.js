2ee6a1484fbbafb60840803c3da021909694055d
gjacob-frsf <gjacob@frsf.utn.edu.ar>
2019-10-15 20:22:16 -0300

    63 Setup test env with jest and update user validation unit test

---
+++ server/tests/models/user.model.test.js
@@ -0,0 +1,80 @@
+const UserModel = require("../../src/models/user.model");
+
+test('Valid user, all fields required filled', () => {
+  var resp = UserModel.validateUser(new UserModel({
+    username: "testUserName",
+    email: "testUser@argeniss.com",
+    name: "testName",
+    surname: "testSurname",
+    birthday: '01-01-1990',
+  }));
+
+  expect(resp.error).toBeUndefined()
+});
+
+test('Invalid user, username is required', () => {
+  var resp = UserModel.validateUser(new UserModel({
+    email: "testUser@argeniss.com",
+    name: "testName",
+    surname: "testSurname",
+    birthday: '01-01-1990',
+  }));
+
+  expect(resp.error.details[0].message).toBe('"username" is required')
+});
+
+test('Invalid user, email is required', () => {
+  var resp = UserModel.validateUser(new UserModel({
+    username: "testUserName",
+    name: "testName",
+    surname: "testSurname",
+    birthday: '01-01-1990',
+  }));
+
+  expect(resp.error.details[0].message).toBe('"email" is required')
+});
+
+test('Invalid user, name is required', () => {
+  var resp = UserModel.validateUser(new UserModel({
+    username: "testUserName",
+    email: "testUser@argeniss.com",
+    surname: "testSurname",
+    birthday: '01-01-1990',
+  }));
+
+  expect(resp.error.details[0].message).toBe('"name" is required')
+});
+
+test('Invalid user, surname is required', () => {
+  var resp = UserModel.validateUser(new UserModel({
+    username: "testUserName",
+    email: "testUser@argeniss.com",
+    name: "testName",
+    birthday: '01-01-1990',
+  }));
+
+  expect(resp.error.details[0].message).toBe('"surname" is required')
+});
+
+test('Invalid user, birthday is required', () => {
+  var resp = UserModel.validateUser(new UserModel({
+    username: "testUserName",
+    email: "testUser@argeniss.com",
+    name: "testName",
+    surname: "testSurname",
+  }));
+
+  expect(resp.error.details[0].message).toBe('"birthday" is required')
+});
+
+test('Invalid user, email does not contain @', () => {
+  var resp = UserModel.validateUser(new UserModel({
+    username: "testUserName",
+    email: "testUserargeniss.com",
+    name: "testName",
+    surname: "testSurname",
+    birthday: '01-01-1990',
+  }));
+
+  expect(resp.error).not.toBeUndefined()
+});
